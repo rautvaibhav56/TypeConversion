2076d710f8954246691e3dd32309ae947342b17f
vaibhav <rautvaibhav56@gmail.com>
2020-10-13 18:38:27 +0530

    java type conversion

---
+++ TypeConversion/src/TypeConversion1.java
@@ -0,0 +1,19 @@
+//TypeConversion
+public class TypeConversion1 {
+
+	public static void main(String[] args) {
+		 int i = 100;  
+         
+	        //type conversion 
+	        long l = i;  
+	          
+	        //type conversion 
+	        float f = l;  
+	        System.out.println("Int value "+i); 
+	        System.out.println("Long value "+l); 
+	        System.out.println("Float value "+f); 
+	    } 
+
+	}
+
+
